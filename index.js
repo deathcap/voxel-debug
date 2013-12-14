34bacdb5e8ab546863b5d0ab832e4571b9fdd348
deathcap <deathcap@gmx.co.uk>
2013-12-14 12:19:26 -0800

    Add option to change texture pack

--- index.js
+++ index.js
@@ -158,4 +158,13 @@ Debug.prototype._render = function() {
     self.game.materials = self.game.materials.reconfigure();
     self.game.showAllChunks() 
   });
+
+  this.texturePack = this.game.materials.texturePath.split('/')[1];
+  folder.add(this, 'texturePack').onFinishChange(function(value) {
+    self.game.materials.opts.texturePath = 'AssetPacks/' + value + '/textures/blocks/';
+    self.game.materials = self.game.materials.reconfigure();
+    var mine = self.game.plugins && self.game.plugins.get('mine');
+    if (mine) mine.setupTextures();
+    self.game.showAllChunks();
+  });
 }
